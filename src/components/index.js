f0cb1adfb81b1d79695c80b38b4485434d028946
APunchvedi <punchvedi.akash96@gmail.com>
2024-12-15 10:23:52 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ReviewCard from "./ReviewCard";
+import ServiceCard from "./ServiceCard";
+
+
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
+
+
---
+++ src/sections/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Hero from "./Hero.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+import Footer from "./Footer.jsx";
+import PopularProducts from "./PopularProducts.jsx";
+import Nav from "../components/Nav.jsx";
+import Services from "./Services.jsx";
+import SpecialOffer from "./SpecialOffer.jsx";
+import Subscribe from "./Subscribe.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+import PopularProductCard from "../components/PopularProductCard.jsx";
+
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Nav,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProductCard
+}
